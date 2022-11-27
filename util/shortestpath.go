7a24d0ca1d9a56dc4bb894147b9ea5545201b343
paulc <212168+paulc@users.noreply.github.com>
2022-11-27 16:36:52 +0000

    path

--- util/shortestpath.go
+++ util/shortestpath.go
@@ -1,31 +1,136 @@
 package util
 
-type Prioritised interface {
-	Priority() float64
+import (
+	"container/heap"
+	"fmt"
+	"math"
+	"strings"
+
+	"golang.org/x/exp/slices"
+)
+
+type Path struct {
+	To   string
+	Cost float64
 }
 
-type PriorityQueue[T Prioritised] []T
+type Graph map[string][]Path
 
-func (q PriorityQueue[T]) Len() int {
-	return len(q)
+func (g Graph) String() string {
+	out := []string{}
+	for k, v := range g {
+		out = append(out, fmt.Sprintf("%s -> %v", k, v))
+	}
+	slices.Sort(out)
+	return strings.Join(out, "\n")
 }
 
-func (q PriorityQueue[T]) Less(i, j int) bool {
-	return (q[i]).Priority() < (q[j]).Priority()
+func (g *Graph) ShortestPath(start, end string) float64 {
+	Q := make(map[string]struct{})
+	for k, _ := range *g {
+		Q[k] = struct{}{}
+	}
+	known := make(map[string]struct{})
+	dist := make(map[string]float64)
+	dist[start] = 0
+	known[start] = struct{}{}
+	for len(Q) > 0 {
+		// fmt.Println("Keys:", maps.Keys(Q))
+		// fmt.Println("Known:", maps.Keys(known))
+		// fmt.Println("Dist:", dist)
+		var u string
+		min := math.Inf(1)
+		for k, _ := range known {
+			if dist[k] < min {
+				min = dist[k]
+				u = k
+			}
+		}
+		// fmt.Println("u >>", u)
+		delete(Q, u)
+		delete(known, u)
+		if u == end {
+			break
+		}
+		for _, v := range (*g)[u] {
+			if _, found := Q[v.To]; found {
+				known[v.To] = struct{}{}
+				if _, found := dist[v.To]; !found {
+					dist[v.To] = math.Inf(1)
+				}
+				if alt := dist[u] + v.Cost; alt < dist[v.To] {
+					dist[v.To] = alt
+				}
+			}
+		}
+	}
+	return dist[end]
 }
 
-func (q PriorityQueue[T]) Swap(i, j int) {
-	q[i], q[j] = q[j], q[i]
+type PathQ []Path
+
+func (q PathQ) Len() int           { return len(q) }
+func (q PathQ) Less(i, j int) bool { return q[i].Cost < q[j].Cost }
+func (q PathQ) Swap(i, j int)      { q[i], q[j] = q[j], q[i] }
+
+func (q *PathQ) Push(x any) {
+	*q = append(*q, x.(Path))
 }
 
-func (q *PriorityQueue[T]) Push(x any) {
-	*q = append(*q, x.(T))
-}
-
-func (q *PriorityQueue[T]) Pop() any {
+func (q *PathQ) Pop() any {
 	old := *q
 	n := len(old)
 	x := old[n-1]
 	*q = old[0 : n-1]
 	return x
+}
+
+func (q *PathQ) UpdateCost(dest string, cost float64) {
+	for i, _ := range *q {
+		if (*q)[i].To == dest {
+			(*q)[i].Cost = cost
+			heap.Fix(q, i)
+			return
+		}
+	}
+}
+
+func (g *Graph) CalculatePaths(start string) (map[string]float64, map[string]string) {
+	cost := make(map[string]float64)
+	cost[start] = 0
+
+	prev := make(map[string]string)
+	pathQ := &PathQ{}
+	heap.Init(pathQ)
+
+	for k, _ := range *g {
+		if k != start {
+			cost[k] = math.Inf(1)
+			prev[k] = ""
+		}
+		heap.Push(pathQ, Path{k, cost[k]})
+	}
+
+	for len(*pathQ) > 0 {
+		u := heap.Pop(pathQ).(Path)
+		for _, v := range (*g)[u.To] {
+			if alt := cost[u.To] + v.Cost; alt < cost[v.To] {
+				cost[v.To] = alt
+				prev[v.To] = u.To
+				pathQ.UpdateCost(v.To, alt)
+			}
+		}
+	}
+	return cost, prev
+}
+
+func (g *Graph) Route(start, end string) (float64, []string) {
+	cost, prev := g.CalculatePaths(start)
+	cur := end
+	route := []string{end}
+	for cur != start {
+		cur = prev[cur]
+		route = append(route, cur)
+	}
+	return cost[end], route
 }
--- util/shortestpath_test.go
+++ util/shortestpath_test.go
@@ -1,26 +1,144 @@
 package util
 
 import (
+	"bytes"
 	"container/heap"
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
 	"testing"
 
 	"golang.org/x/exp/slices"
 )
 
-type _i int
+func TestPathQ(t *testing.T) {
+	pathQ := &PathQ{}
+	heap.Init(pathQ)
 
-func (i _i) Priority() float64 {
-	return float64(i)
+	heap.Push(pathQ, Path{"A", 5})
+	heap.Push(pathQ, Path{"B", 3})
+	heap.Push(pathQ, Path{"C", 99})
+	heap.Push(pathQ, Path{"D", 1})
+
+	out := []string{}
+	for len(*pathQ) > 0 {
+		out = append(out, heap.Pop(pathQ).(Path).To)
+	}
+
+	if !slices.Equal(out, []string{"D", "B", "A", "C"}) {
+		t.Error(out)
+	}
+
+	heap.Push(pathQ, Path{"A", 5})
+	heap.Push(pathQ, Path{"B", 3})
+	heap.Push(pathQ, Path{"C", 99})
+	heap.Push(pathQ, Path{"D", 1})
+	pathQ.UpdateCost("C", 2)
+
+	out = out[:0]
+	for len(*pathQ) > 0 {
+		out = append(out, heap.Pop(pathQ).(Path).To)
+	}
+
+	if !slices.Equal(out, []string{"D", "C", "B", "A"}) {
+		t.Error(out)
+	}
 }
 
-func TestPriorityQueue(t *testing.T) {
-	q := &PriorityQueue[_i]{}
-	heap.Init(q)
-	heap.Push(q, _i(5))
-	heap.Push(q, _i(10))
-	heap.Push(q, _i(1))
-	heap.Push(q, _i(3))
-	if !slices.Equal(*q, []_i{_i(1), _i(3), _i(5), _i(10)}) {
-		t.Error(*q)
+// From aoc2021/day15
+const path_test = `
+1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581
+`
+
+func makeGraph(r io.Reader) (*Graph, error) {
+	a, err := ArrayReader(r, MakeStringSplitter(""), strconv.Atoi)
+	if err != nil {
+		return nil, err
+	}
+	g := make(Graph)
+	a.Each(func(e ArrayElement[int]) {
+		adj := []Path{}
+		for _, v := range a.Adjacent(e.x, e.y) {
+			adj = append(adj, Path{fmt.Sprintf("%d:%d", v.x, v.y), float64(v.val)})
+		}
+		g[fmt.Sprintf("%d:%d", e.x, e.y)] = adj
+	})
+	return &g, nil
+}
+
+func TestShortestPath(t *testing.T) {
+	g, err := makeGraph(bytes.NewBufferString(strings.TrimSpace(path_test)))
+	if err != nil {
+		t.Fatal(err)
+	}
+	sp := g.ShortestPath("0:0", "9:9")
+	if sp != 40 {
+		t.Error("SP:", sp)
+	}
+}
+
+func TestCalculatePaths(t *testing.T) {
+	g, err := makeGraph(bytes.NewBufferString(strings.TrimSpace(path_test)))
+	if err != nil {
+		t.Fatal(err)
+	}
+	cost, prev := g.CalculatePaths("0:0")
+	if cost["9:9"] != 40 {
+		t.Error("cost:", cost, "\nprev:", prev)
+	}
+}
+
+func TestRoute(t *testing.T) {
+	g, err := makeGraph(bytes.NewBufferString(strings.TrimSpace(path_test)))
+	if err != nil {
+		t.Fatal(err)
+	}
+	cost, route := g.Route("0:0", "9:9")
+	if cost != 40 {
+		t.Error("cost:", cost)
+	}
+	expected := []string{"9:9", "9:8", "8:8", "8:7", "8:6", "8:5", "7:5", "7:4", "7:3", "6:3", "6:2", "5:2", "4:2", "3:2", "2:2", "1:2", "0:2", "0:1", "0:0"}
+	if !slices.Equal(route, expected) {
+		t.Error("route:", route)
+	}
+}
+
+func TestShortestPathBig(t *testing.T) {
+	r, err := UrlOpen("testdata/input.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	g, err := makeGraph(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	sp := g.ShortestPath("0:0", "99:99")
+	if sp != 602 {
+		t.Error("cost:", sp)
+	}
+}
+
+func TestRouteBig(t *testing.T) {
+	r, err := UrlOpen("testdata/input.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	g, err := makeGraph(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	cost, _ := g.Route("0:0", "99:99")
+	if cost != 602 {
+		t.Error("cost:", cost)
 	}
 }
