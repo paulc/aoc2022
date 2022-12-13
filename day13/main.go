bcfd53d68f8a409be67c795428905c3edb953224
paulc <212168+paulc@users.noreply.github.com>
2022-12-13 09:54:51 +0000

    day13

---
+++ day13/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+
+	"github.com/paulc/aoc2022/util"
+	"github.com/paulc/aoc2022/util/array"
+)
+
+func parseInput(r io.Reader) array.Array[int] {
+	return util.Must(array.ArrayReader(r, array.SplitWS, func(s string) (int, error) { return strconv.Atoi(s) }))
+}
+
+func part1(input array.Array[int]) (result int) {
+	return result
+}
+
+func part2(input array.Array[int]) (result int) {
+	return result
+}
+
+func main() {
+	input := parseInput(util.Must(os.Open("input")))
+	fmt.Println("Part1:", part1(input))
+	fmt.Println("Part2:", part2(input))
+}
