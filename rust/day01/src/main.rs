04bba90e03745ab1e5e8ff6d1503488017611f87
paulc <212168+paulc@users.noreply.github.com>
2023-06-07 23:10:41 +0100

    day01

--- rust/day01/src/main.rs
+++ rust/day01/src/main.rs
@@ -1,26 +1,67 @@
 
-use std::io::BufReader;
-use std::io::prelude::*;
 use std::fs::File;
+use std::io::prelude::*;
+
+fn parse_input(input: &mut impl Read) -> Result<Vec<Vec<i32>>, std::io::Error> {
+    let mut s = String::new();
+    input.read_to_string(&mut s)?;
+    Ok(s.lines().collect::<Vec<_>>()
+                    .split(|line| line.is_empty())
+                    .map(|group| { group
+                                    .iter()
+                                    .map(|v| v.parse::<i32>().unwrap())
+                                    .collect::<Vec<_>>()
+                    })
+                    .collect::<Vec<_>>()
+    )
+}
+
+fn part1(input: &Vec<Vec<i32>>) -> Option<i32> {
+    input.iter().map(|group| group.iter().sum()).max()
+}
+
+fn part2(input: &Vec<Vec<i32>>) -> Option<i32> {
+    let mut groups = input.iter().map(|group| group.iter().sum::<i32>()).collect::<Vec<_>>();
+    groups.sort();
+    Some(groups.iter().rev().take(3).sum::<i32>())
+}
 
 fn main() -> std::io::Result<()> {
-    let f = BufReader::new(File::open("input")?);
-    let mut current: i32 = 0;
-    let mut total: Vec<i32> = Vec::new();
-    for l in f.lines() {
-        let l = l?;
-        if l.trim().is_empty() {
-            total.push(current);
-            current = 0;
-        } else {
-            current += l.trim().parse::<i32>().unwrap();
-        }
-    }
-    if current > 0 {
-        total.push(current);
-    }
-    total.sort();
-    println!("Part 1: {}",total.iter().rev().take(1).sum::<i32>());
-    println!("Part 2: {}",total.iter().rev().take(3).sum::<i32>());
+
+    let mut f = File::open("input")?;
+    let data = parse_input(&mut f)?;
+    println!("Part1: {:?}",part1(&data).unwrap());
+    println!("Part2: {:?}",part2(&data).unwrap());
+
     Ok(())
 }
+
+const TESTDATA : &str = "
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+";
+
+#[test]
+fn test_part1() {
+    let data = parse_input(&mut TESTDATA.trim().as_bytes()).unwrap();
+    assert_eq!(part1(&data).unwrap(), 24000);
+}
+
+#[test]
+fn test_part2() {
+    let data = parse_input(&mut TESTDATA.trim().as_bytes()).unwrap();
+    assert_eq!(part2(&data).unwrap(), 45000);
+}
+
