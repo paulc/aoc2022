269919156d084c7300c8997c77d5bef5c5ab3be5
paulc <212168+paulc@users.noreply.github.com>
2023-06-21 18:35:48 +0100

    day09

--- rust/day09/src/main.rs
+++ rust/day09/src/main.rs
@@ -1,6 +1,8 @@
 #![allow(unused)]
 
+use std::cmp::Ordering;
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
@@ -14,44 +16,122 @@ const PART2_RESULT: Out = 1;
 const PART2_RESULT2: Out = 36;
 
 #[derive(Debug)]
-enum Move {
-    Right(usize),
-    Left(usize),
-    Up(usize),
-    Down(usize),
+enum Direction {
+    Right,
+    Left,
+    Up,
+    Down,
+}
+#[derive(Debug)]
+struct Move {
+    d: Direction,
+    n: usize,
 }
 
 impl TryFrom<&str> for Move {
     type Error = std::io::Error;
     fn try_from(s: &str) -> Result<Self, Self::Error> {
-        fn parse_usize(s: &str) -> Result<usize, std::io::Error> {
-            s.parse::<usize>().map_err(|e| Error::new(InvalidData, s))
-        }
+        let parse_usize = |s: &str| s.parse::<usize>().map_err(|e| Error::new(InvalidData, s));
         match s.split_once(" ") {
-            Some(("R", s)) => Ok(Move::Right(parse_usize(s)?)),
-            Some(("L", s)) => Ok(Move::Left(parse_usize(s)?)),
-            Some(("U", s)) => Ok(Move::Up(parse_usize(s)?)),
-            Some(("D", s)) => Ok(Move::Down(parse_usize(s)?)),
+            Some(("R", s)) => Ok(Move {
+                d: Direction::Right,
+                n: parse_usize(s)?,
+            }),
+            Some(("L", s)) => Ok(Move {
+                d: Direction::Left,
+                n: parse_usize(s)?,
+            }),
+            Some(("U", s)) => Ok(Move {
+                d: Direction::Up,
+                n: parse_usize(s)?,
+            }),
+            Some(("D", s)) => Ok(Move {
+                d: Direction::Down,
+                n: parse_usize(s)?,
+            }),
             _ => Err(Error::new(InvalidData, s)),
         }
     }
 }
 
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Default, Hash)]
+struct XY(i32, i32);
+
+impl XY {
+    fn is_touching(&self, other: &XY) -> bool {
+        ((self.0 - other.0).abs() <= 1) && ((self.1 - other.1).abs() <= 1)
+    }
+    fn add(&self, other: &XY) -> XY {
+        XY(self.0 + other.0, self.1 + other.1)
+    }
+    fn cmp(&self, other: &XY) -> (Ordering, Ordering) {
+        (self.0.cmp(&(other.0)), self.1.cmp(&(other.1)))
+    }
+}
+
+#[derive(Debug)]
+struct Rope {
+    knots: Vec<XY>,
+    visited: HashSet<XY>,
+}
+
+impl Rope {
+    fn new(n: usize) -> Self {
+        Self {
+            knots: vec![XY::default(); n],
+            visited: HashSet::new(),
+        }
+    }
+    fn move_rope(&mut self, m: &Move) {
+        for _ in 0..m.n {
+            // Move head
+            match m.d {
+                Direction::Right => self.knots[0] = self.knots[0].add(&XY(1, 0)),
+                Direction::Left => self.knots[0] = self.knots[0].add(&XY(-1, 0)),
+                Direction::Up => self.knots[0] = self.knots[0].add(&XY(0, 1)),
+                Direction::Down => self.knots[0] = self.knots[0].add(&XY(0, -1)),
+            }
+            // Move tail
+            for i in 1..self.knots.len() {
+                if !self.knots[i].is_touching(&self.knots[i - 1]) {
+                    let (cmp_x, cmp_y) = self.knots[i - 1].cmp(&self.knots[i]);
+                    let offset = XY(
+                        match cmp_x {
+                            Ordering::Less => -1,
+                            Ordering::Equal => 0,
+                            Ordering::Greater => 1,
+                        },
+                        match cmp_y {
+                            Ordering::Less => -1,
+                            Ordering::Equal => 0,
+                            Ordering::Greater => 1,
+                        },
+                    );
+                    self.knots[i] = self.knots[i].add(&offset);
+                }
+            }
+            self.visited.insert(self.knots[self.knots.len() - 1]);
+        }
+    }
+}
+
 fn parse_input(input: &mut impl Read) -> std::io::Result<In> {
-    let reader = BufReader::new(input);
-    reader
+    BufReader::new(input)
         .lines()
         .map(|l| Move::try_from(l?.as_str()))
         .collect::<std::io::Result<Vec<Move>>>()
 }
 
 fn part1(input: &In) -> Out {
-    println!("{:?}", input);
-    PART1_RESULT
+    let mut rope = Rope::new(2);
+    input.iter().for_each(|m| rope.move_rope(m));
+    rope.visited.len()
 }
 
 fn part2(input: &In) -> Out {
-    PART2_RESULT
+    let mut rope = Rope::new(10);
+    input.iter().for_each(|m| rope.move_rope(m));
+    rope.visited.len()
 }
 
 fn main() -> std::io::Result<()> {
